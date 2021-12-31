1f1653b5465df0afdcd420eafddcdef2941fe3c0
Hanif-2610 <60161283+Hanif-2610@users.noreply.github.com>
2021-12-31 22:11:03 +0800

    Challenge Complete

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float currentIntervalFromNextDogSpawn;
+    private float minIntervalFromNextDogSpawnInSec = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Subtract the time passed from each frame from the current counter of time before next dog spawn
+        if (currentIntervalFromNextDogSpawn > 0)
+            currentIntervalFromNextDogSpawn -= Time.deltaTime;
+
+        // On spacebar press, send dog if time passed before previous spawn dog
+        if (Input.GetKeyDown(KeyCode.Space) && currentIntervalFromNextDogSpawn <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            currentIntervalFromNextDogSpawn = minIntervalFromNextDogSpawnInSec;
+        }
+    }
+}
